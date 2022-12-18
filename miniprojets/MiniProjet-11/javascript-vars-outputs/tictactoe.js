62d715c48ac836f308bc21538a33d2f07130f761
Mohamed Anis MANI <manimohamed@gmail.com>
2022-12-18 13:48:45 +0100

    tictactoe

--- miniprojets/MiniProjet-11/javascript-vars-outputs/tictactoe.js
+++ miniprojets/MiniProjet-11/javascript-vars-outputs/tictactoe.js
@@ -1,79 +1,89 @@
-const game = document.getElementById("game");
-const boxes = [...game.querySelectorAll(".box")];
-const content = Array(9).fill("");
-
-let letter = "X";
-let gameEnded = false;
-
-function concatSpaced(content, start, space) {
+function generateBoxes(game, nSize) {
   let s = "";
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < nSize * nSize; i++) {
+    s += `<div class="box cols${nSize} emptycolor"></div>`;
+  }
+  game.innerHTML = s;
+}
+
+function concatSpaced(content, nSize, start, space) {
+  let s = "";
+  for (let i = 0; i < nSize; i++) {
     s += content[start + space * i];
   }
   return s;
 }
 
-function getRow(content, num) {
-  return concatSpaced(content, num * 3, 1);
+function getRow(content, nSize, num) {
+  return concatSpaced(content, nSize, num * nSize, 1);
 }
 
-function getCol(content, num) {
-  return concatSpaced(content, num, 3);
+function getCol(content, nSize, num) {
+  return concatSpaced(content, nSize, num, nSize);
 }
 
-function getDiag1(content) {
-  return concatSpaced(content, 0, 4);
+function getDiag1(content, nSize, num) {
+  return concatSpaced(content, nSize - Math.abs(num), (num < 0) ? (nSize * -num) : num, nSize + 1);
 }
 
-function getDiag2(content) {
-  return concatSpaced(content, 2, 2);
+function getDiag2(content, nSize, num) {
+  return concatSpaced(content, nSize - Math.abs(num), (num < 0) ? (nSize * -num + 4) : (nSize - num - 1), nSize - 1);
 }
 
-function getWinner(val) {
-  if (val == "XXX") {
-    return "X";
-  } else if (val == "OOO") {
-    return "O";
+function getWinner(val, nWinSize) {
+  let i = 0;
+  while (i < val.length) {
+    let j = i;
+    while (j < val.length && val[j] == val[i]) {
+      j++;
+    }
+    if (j - i >= nWinSize) {
+      return val[i];
+    } else {
+      i = j;
+    }
   }
   return "";
 }
 
-function verifWinner(content) {
-  let winner = getWinner(getDiag1(content));
-  if (winner != "") return winner;
-  winner = getWinner(getDiag2(content));
-  if (winner != "") return winner;
-  for (let i = 0; i < 3; i++) {
-    winner = getWinner(getRow(content, i));
+function verifWinner(content, nWinSize, nSize) {
+  for (let i = -nSize + 1; i < nSize; i++) {
+    let winner = getWinner(getDiag1(content, nSize, i), nWinSize);
     if (winner != "") return winner;
-    winner = getWinner(getCol(content, i));
+    winner = getWinner(getDiag2(content, nSize, i), nWinSize);
+    if (winner != "") return winner;
+    if (i < 0) continue;
+    winner = getWinner(getRow(content, nSize, i), nWinSize);
+    if (winner != "") return winner;
+    winner = getWinner(getCol(content, nSize, i), nWinSize);
     if (winner != "") return winner;
   }
   return "";
 }
 
-function isTie(content) {
+function isTie(content, nSize) {
   let s = "";
-  for (let i = 0; i < 3; i++) {
-    s += getRow(content, i);
+  for (let i = 0; i < nSize; i++) {
+    s += getRow(content, nSize, i);
   }
-  return s.length == 9;
+  return s.length == nSize * nSize;
 }
 
-function resetGame() {
-  gameEnded = false;
+function resetGame(game, nSize) {
+  generateBoxes(game, nSize);
+  boxes = [...game.querySelectorAll(".box")];
+  content = Array(nSize * nSize).fill("");
   letter = "X";
-  for (let i = 0; i < boxes.length; i++) {
-    boxes[i].innerHTML = "";
-    boxes[i].className = "box emptycolor";
-    content[i] = "";
+  gameEnded = false;
+  for (let i = 0; i < nSize * nSize; i++) {
+    boxes[i].addEventListener("click", boxClicked);
   }
 }
 
 function boxClicked(e) {
   const boxIndex = boxes.indexOf(e.target);
   if (gameEnded) {
-    resetGame();
+    resetGame(game, nSize);
   }
 
   if (content[boxIndex] != "") {
@@ -82,13 +92,14 @@ function boxClicked(e) {
 
   content[boxIndex] = letter;
   boxes[boxIndex].innerHTML = letter;
-  boxes[boxIndex].className = "box " + letter + "color";
+  boxes[boxIndex].classList.remove("emptycolor");
+  boxes[boxIndex].classList.add(letter + "color");
 
-  const winner = verifWinner(content);
+  const winner = verifWinner(content, nWinSize, nSize);
   if (winner != "") {
     gameEnded = true;
     setTimeout(() => { alert(winner + " wins."); }, 10);
-  } else if (isTie(content)) {
+  } else if (isTie(content, nSize)) {
     gameEnded = true;
     setTimeout(() => { alert("Tie game."); }, 10);
   }
@@ -96,6 +107,9 @@ function boxClicked(e) {
   letter = (letter == "O") ? "X" : "O";
 }
 
-for (let i = 0; i < 9; i++) {
-  boxes[i].addEventListener("click", boxClicked);
-}
+let nSize = 6;
+let nWinSize = 3;
+const game = document.getElementById("game");
+resetGame(game, nSize);
+
+
