4fc2a09d51f7d1e35cffc1e632659cd26454688a
Mohamed Anis MANI <manimohamed@gmail.com>
2024-04-17 06:24:48 +0100

    Update mp16

--- miniprojets/MiniProjet-19/extract_pixels.py
+++ miniprojets/MiniProjet-19/extract_pixels.py
@@ -3,7 +3,7 @@ from pprint import pprint
 from PIL import Image
 
 currdir = os.path.dirname(__file__)
-filepath = os.path.join(currdir, 'pixel-art-naruto-facile-4.jpg')
+filepath = os.path.join(currdir, 'pixel-art-mario.jpg')#'pixel-art-naruto-facile-4.jpg')
 
 # Open an image file
 with Image.open(filepath) as img:
@@ -13,16 +13,16 @@ with Image.open(filepath) as img:
     # Get the size of the image
     width, height = img.size
 
-    xstep, ystep = 20, 20
+    xstep, ystep = 45, 45
     
     # Create a list to hold the pixel colors
     pixel_colors = []
 
     distinct_colors = []
     # Loop over each pixel
-    for x in range(0, width, xstep):
+    for y in range(0, height, ystep):
         line_colors = []
-        for y in range(0, height, ystep):
+        for x in range(0, width, xstep):
             # Get the color of the pixel
             pixel_color = img.getpixel((x + xstep // 2, y + ystep // 2))
             rgb_color = f"#{hex(pixel_color[0])[2:]:02}{hex(pixel_color[1])[2:]:02}{hex(pixel_color[2])[2:]:02}"
